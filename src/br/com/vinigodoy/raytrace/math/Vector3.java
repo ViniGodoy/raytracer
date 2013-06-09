2cdc3fed9c72899739f51eba44d777febdf55b3a
Vinicius Godoy <vinigodoy@gmail.com>
2013-06-09 11:59:49 -0300

    Added reflect method.

--- src/br/com/vinigodoy/raytrace/math/Vector3.java
+++ src/br/com/vinigodoy/raytrace/math/Vector3.java
@@ -2,24 +2,26 @@ package br.com.vinigodoy.raytrace.math;
 
 import java.awt.*;
 
-/******************************************************************************
- *
+/**
+ * ***************************************************************************
+ * <p/>
  * COPYRIGHT Vinícius G. Mendonça ALL RIGHTS RESERVED.
- *
+ * <p/>
  * This software cannot be copied, stored, distributed without
  * Vinícius G.Mendonça prior authorization.
- *
+ * <p/>
  * is free to be redistributed or used under Creative Commons license 2.5 br:
  * http://creativecommons.org/licenses/by-sa/2.5/br/
- *
- *******************************************************************************/
+ * <p/>
+ * *****************************************************************************
+ */
 public class Vector3 implements Cloneable {
     public float x;
     public float y;
     public float z;
 
     public Vector3() {
-        this(0,0,0);
+        this(0, 0, 0);
     }
 
     public Vector3(float x, float y, float z) {
@@ -28,39 +30,33 @@ public class Vector3 implements Cloneable {
         this.z = z;
     }
 
-    public float sizeSqr()
-    {
-        return x*x + y*y + z*z;
+    public float sizeSqr() {
+        return x * x + y * y + z * z;
     }
 
-    public float size()
-    {
+    public float size() {
         return (float) Math.sqrt(sizeSqr());
     }
 
-    public Vector3 normalize()
-    {
+    public Vector3 normalize() {
         return divide(size());
     }
 
-    public Vector3 add(Vector3 other)
-    {
+    public Vector3 add(Vector3 other) {
         x += other.x;
         y += other.y;
         z += other.z;
         return this;
     }
 
-    public Vector3 negate()
-    {
+    public Vector3 negate() {
         x = -x;
         y = -y;
         z = -z;
         return this;
     }
 
-    public Vector3 subtract(Vector3 other)
-    {
+    public Vector3 subtract(Vector3 other) {
         x -= other.x;
         y -= other.y;
         z -= other.z;
@@ -79,13 +75,12 @@ public class Vector3 implements Cloneable {
     }
 
     public float dot(Vector3 other) {
-        return x*other.x +
-               y * other.y +
-               z * other.z;
+        return x * other.x +
+                y * other.y +
+                z * other.z;
     }
 
-    public Vector3 cross(Vector3 v)
-    {
+    public Vector3 cross(Vector3 v) {
         return new Vector3(
                 y * v.z - z * v.y,
                 z * v.x - x * v.z,
@@ -103,11 +98,11 @@ public class Vector3 implements Cloneable {
     /**
      * Truncates this vector component sizes to fit in [0,1] range.
      * Useful when this vector represents colors.
+     *
      * @return This vector.
      * @see #normalize()
      */
-    public Vector3 saturate()
-    {
+    public Vector3 saturate() {
         x = x < 0 ? 0 : (x > 1 ? 1 : x);
         y = y < 0 ? 0 : (y > 1 ? 1 : y);
         z = z < 0 ? 0 : (z > 1 ? 1 : z);
@@ -166,17 +161,20 @@ public class Vector3 implements Cloneable {
     /**
      * Multiplicates every component. This is not a standard vector operation, but it's usefull when the vector
      * describes materials and lights.
+     *
      * @param v1 Vector 1
      * @param v2 Vector 2
      * @return The multiplied vectors.
      */
-    public static Vector3 mul(Vector3 v1, Vector3 v2)
-    {
+    public static Vector3 mul(Vector3 v1, Vector3 v2) {
         return new Vector3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
     }
 
-    public static Vector3 saturate(Vector3 vector)
-    {
+    public static Vector3 saturate(Vector3 vector) {
         return vector.clone().saturate();
+    }
+
+    public static Vector3 reflect(Vector3 direction, Vector3 normal) {
+        return subtract(direction, multiply(normal, 2.0f * direction.dot(normal)));
     }
 }
