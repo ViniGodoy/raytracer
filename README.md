c5cda7e4b9c7d99a988640b5234e5b835f224467
ViniGodoy <vinigodoy@gmail.com>
2013-11-03 19:59:59 -0200

    Small fixes.

--- README.md
+++ README.md
@@ -9,13 +9,14 @@ A Simple Java raytracer written in Java 2D.
 The code is based in [Ray Tracing from the Ground Up book][1] and in [this excelent tutorial of flipcode][2].
 With some refactorings, corrections and ideas of my own.
 
-[1]: http://www.raytracegroundup.com/ "Ray tracing from the Ground Up - Kevin Suffern"
-[2]: http://www.flipcode.com/archives/Raytracing_Topics_Techniques-Part_1_Introduction.shtml "Flipcode Raytracing Tutorial"
-
 Change History
 --------------
 
-- 03/12/2013 Restarted from scratch with a new architecture.
+- 03/12/2013 Restarted from scratch with a new architecture. See [screenshot][3].
 - 16/06/2013 Added refraction.
 - 10/06/2013 Refactored ray tracer classes.
 - 02/06/2013 First version of ray tracer.
+
+[1]: http://www.raytracegroundup.com/ "Ray tracing from the Ground Up - Kevin Suffern"
+[2]: http://www.flipcode.com/archives/Raytracing_Topics_Techniques-Part_1_Introduction.shtml "Flipcode Raytracing Tutorial"
+[3]: https://lh6.googleusercontent.com/-lN7F5S4R1LQ/UnbEfeIkyyI/AAAAAAAAAm8/Pameytz-NdQ/w2166-h1218-no/raytracer+HD.png
--- src/br/com/vinigodoy/raytracer/gui/SampleFrame.java
+++ src/br/com/vinigodoy/raytracer/gui/SampleFrame.java
@@ -75,60 +75,63 @@ public class SampleFrame extends JFrame {
 
     private World createScene() {
         world = new World(new TrivialTracer(), new Vector3());
-        // colours
+
+        // colors
+        Vector3 lightGreen = new Vector3(0.65f, 1.0f, 0.30f);
+        Vector3 green = new Vector3(0.0f, 0.6f, 0.3f);
+        Vector3 darkGreen = new Vector3(0.0f, 0.41f, 0.41f);
 
         Vector3 yellow = new Vector3(1.0f, 1.0f, 0.0f);
+        Vector3 darkYellow = new Vector3(0.61f, 0.61f, 0.0f);
+
+        Vector3 lightPurple = new Vector3(0.65f, 0.3f, 1.0f);
+        Vector3 darkPurple = new Vector3(0.5f, 0.0f, 1.0f);
+
         Vector3 brown = new Vector3(0.71f, 0.40f, 0.16f);
-        Vector3 dark_green = new Vector3(0.0f, 0.41f, 0.41f);
         Vector3 orange = new Vector3(1.0f, 0.75f, 0.0f);
-        Vector3 green = new Vector3(0.0f, 0.6f, 0.3f);
-        Vector3 light_green = new Vector3(0.65f, 1.0f, 0.30f);
-        Vector3 dark_yellow = new Vector3(0.61f, 0.61f, 0.0f);
-        Vector3 light_purple = new Vector3(0.65f, 0.3f, 1.0f);
-        Vector3 dark_purple = new Vector3(0.5f, 0.0f, 1.0f);
 
         // spheres
         world.add(new Sphere(new Vector3(5, 3, 0), 30, yellow));
         world.add(new Sphere(new Vector3(45, -7, -60), 20, brown));
-        world.add(new Sphere(new Vector3(40, 43, -100), 17, dark_green));
+        world.add(new Sphere(new Vector3(40, 43, -100), 17, darkGreen));
         world.add(new Sphere(new Vector3(-20, 28, -15), 20, orange));
         world.add(new Sphere(new Vector3(-25, -7, -35), 27, green));
 
-        world.add(new Sphere(new Vector3(20, -27, -35), 25, light_green));
+        world.add(new Sphere(new Vector3(20, -27, -35), 25, lightGreen));
         world.add(new Sphere(new Vector3(35, 18, -35), 22, green));
         world.add(new Sphere(new Vector3(-57, -17, -50), 15, brown));
-        world.add(new Sphere(new Vector3(-47, 16, -80), 23, light_green));
-        world.add(new Sphere(new Vector3(-15, -32, -60), 22, dark_green));
+        world.add(new Sphere(new Vector3(-47, 16, -80), 23, lightGreen));
+        world.add(new Sphere(new Vector3(-15, -32, -60), 22, darkGreen));
 
-        world.add(new Sphere(new Vector3(-35, -37, -80), 22, dark_yellow));
-        world.add(new Sphere(new Vector3(10, 43, -80), 22, dark_yellow));
-        world.add(new Sphere(new Vector3(30, -7, -80), 10, dark_yellow)); //(hidden)
-        world.add(new Sphere(new Vector3(-40, 48, -110), 18, dark_green));
+        world.add(new Sphere(new Vector3(-35, -37, -80), 22, darkYellow));
+        world.add(new Sphere(new Vector3(10, 43, -80), 22, darkYellow));
+        world.add(new Sphere(new Vector3(30, -7, -80), 10, darkYellow)); //(hidden)
+        world.add(new Sphere(new Vector3(-40, 48, -110), 18, darkGreen));
         world.add(new Sphere(new Vector3(-10, 53, -120), 18, brown));
 
-        world.add(new Sphere(new Vector3(-55, -52, -100), 10, light_purple));
+        world.add(new Sphere(new Vector3(-55, -52, -100), 10, lightPurple));
         world.add(new Sphere(new Vector3(5, -52, -100), 15, brown));
-        world.add(new Sphere(new Vector3(-20, -57, -120), 15, dark_purple));
-        world.add(new Sphere(new Vector3(55, -27, -100), 17, dark_green));
+        world.add(new Sphere(new Vector3(-20, -57, -120), 15, darkPurple));
+        world.add(new Sphere(new Vector3(55, -27, -100), 17, darkGreen));
         world.add(new Sphere(new Vector3(50, -47, -120), 15, brown));
 
-        world.add(new Sphere(new Vector3(70, -42, -150), 10, light_purple));
-        world.add(new Sphere(new Vector3(5, 73, -130), 12, light_purple));
-        world.add(new Sphere(new Vector3(66, 21, -130), 13, dark_purple));
-        world.add(new Sphere(new Vector3(72, -12, -140), 12, light_purple));
+        world.add(new Sphere(new Vector3(70, -42, -150), 10, lightPurple));
+        world.add(new Sphere(new Vector3(5, 73, -130), 12, lightPurple));
+        world.add(new Sphere(new Vector3(66, 21, -130), 13, darkPurple));
+        world.add(new Sphere(new Vector3(72, -12, -140), 12, lightPurple));
         world.add(new Sphere(new Vector3(64, 5, -160), 11, green));
 
-        world.add(new Sphere(new Vector3(55, 38, -160), 12, light_purple));
-        world.add(new Sphere(new Vector3(-73, -2, -160), 12, light_purple));
-        world.add(new Sphere(new Vector3(30, -62, -140), 15, dark_purple));
-        world.add(new Sphere(new Vector3(25, 63, -140), 15, dark_purple));
-        world.add(new Sphere(new Vector3(-60, 46, -140), 15, dark_purple));
+        world.add(new Sphere(new Vector3(55, 38, -160), 12, lightPurple));
+        world.add(new Sphere(new Vector3(-73, -2, -160), 12, lightPurple));
+        world.add(new Sphere(new Vector3(30, -62, -140), 15, darkPurple));
+        world.add(new Sphere(new Vector3(25, 63, -140), 15, darkPurple));
+        world.add(new Sphere(new Vector3(-60, 46, -140), 15, darkPurple));
 
-        world.add(new Sphere(new Vector3(-30, 68, -130), 12, light_purple));
+        world.add(new Sphere(new Vector3(-30, 68, -130), 12, lightPurple));
         world.add(new Sphere(new Vector3(58, 56, -180), 11, green));
         world.add(new Sphere(new Vector3(-63, -39, -180), 11, green));
-        world.add(new Sphere(new Vector3(46, 68, -200), 10, light_purple));
-        world.add(new Sphere(new Vector3(-3, -72, -130), 12, light_purple));
+        world.add(new Sphere(new Vector3(46, 68, -200), 10, lightPurple));
+        world.add(new Sphere(new Vector3(-3, -72, -130), 12, lightPurple));
 
         return world;
     }
