a0459ba614151bbfff375f11533d392b39a000d6
ViniGodoy <vinigodoy@gmail.com>
2013-11-11 00:37:25 -0200

    Float reference class.

---
+++ src/br/com/vinigodoy/raytracer/utility/FloatRef.java
@@ -0,0 +1,19 @@
+/*===========================================================================
+COPYRIGHT 2013 Vinícius G. Mendonça ALL RIGHTS RESERVED.
+
+This software cannot be copied, stored, distributed without
+Vinícius G. Mendonça prior authorization.
+
+This file was made available on https://github.com/ViniGodoy and it
+is free to be redistributed or used under Creative Commons license 2.5 br:
+http://creativecommons.org/licenses/by-sa/2.5/br/
+============================================================================*/
+
+package br.com.vinigodoy.raytracer.utility;
+
+/**
+ * A class to allow passing a single floating point value by reference.
+ */
+public class FloatRef {
+    public float value;
+}
