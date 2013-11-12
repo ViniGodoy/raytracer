3ecad4488ab1f6aa8e750b4403e653997d8b6ac5
ViniGodoy <vinigodoy@gmail.com>
2013-11-12 00:03:33 -0200

    Shading support

--- src/br/com/vinigodoy/raytracer/tracer/Raycasting.java
+++ src/br/com/vinigodoy/raytracer/tracer/Raycasting.java
@@ -8,16 +8,25 @@ This file was made available on https://github.com/ViniGodoy and it
 is free to be redistributed or used under Creative Commons license 2.5 br:
 http://creativecommons.org/licenses/by-sa/2.5/br/
 ============================================================================*/
-
 package br.com.vinigodoy.raytracer.tracer;
 
 import br.com.vinigodoy.raytracer.math.Ray;
 import br.com.vinigodoy.raytracer.math.Vector3;
 import br.com.vinigodoy.raytracer.scene.World;
+import br.com.vinigodoy.raytracer.utility.ShadeRec;
 
 /**
- * Represent a tracing strategy
+ * Simple ray casting algorithm
  */
-public interface Tracer {
-    Vector3 trace(World world, Ray ray, int depth);
+public class Raycasting implements Tracer {
+
+    @Override
+    public Vector3 trace(World world, Ray ray, int depth) {
+        ShadeRec sr = world.hit(ray).clone();
+        if (sr.hitAnObject) {
+            sr.ray = ray;
+            return sr.material.shade(sr);
+        }
+        return world.getBackgroundColor();
+    }
 }
