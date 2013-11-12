befdaad80d324579abfdf5dcb759e1b925010bb6
ViniGodoy <vinigodoy@gmail.com>
2013-11-12 00:03:18 -0200

    Shading support

--- src/br/com/vinigodoy/raytracer/scene/World.java
+++ src/br/com/vinigodoy/raytracer/scene/World.java
@@ -12,6 +12,8 @@ http://creativecommons.org/licenses/by-sa/2.5/br/
 package br.com.vinigodoy.raytracer.scene;
 
 import br.com.vinigodoy.raytracer.camera.Camera;
+import br.com.vinigodoy.raytracer.light.AmbientLight;
+import br.com.vinigodoy.raytracer.light.Light;
 import br.com.vinigodoy.raytracer.math.Ray;
 import br.com.vinigodoy.raytracer.math.Vector3;
 import br.com.vinigodoy.raytracer.math.geometry.GeometricObject;
@@ -20,6 +22,7 @@ import br.com.vinigodoy.raytracer.utility.ShadeRec;
 
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static br.com.vinigodoy.raytracer.math.geometry.GeometricObject.HitResult;
@@ -31,6 +34,10 @@ public class World {
 
     private List<GeometricObject> objects = new ArrayList<>();
 
+    private Light ambientLight = new AmbientLight(1.0f, new Vector3(1.0f, 1.0f, 1.0f));
+
+    private List<Light> lights = new ArrayList<>();
+
     public World(Tracer tracer, Vector3 backgroundColor, Camera camera) {
         this.tracer = tracer;
         this.backgroundColor = backgroundColor;
@@ -39,6 +46,10 @@ public class World {
 
     public void add(GeometricObject obj) {
         objects.add(obj);
+    }
+
+    public void add(Light light) {
+        lights.add(light);
     }
 
     public BufferedImage render(ViewPlane vp) {
@@ -51,6 +62,9 @@ public class World {
 
     public ShadeRec hit(Ray ray) {
         ShadeRec sr = new ShadeRec(this);
+
+        Vector3 normal = null;
+        Vector3 localHitPoint = null;
         float tMin = Float.MAX_VALUE;
 
         for (GeometricObject obj : objects) {
@@ -58,13 +72,32 @@ public class World {
             if (hr.isHit() && hr.getT() < tMin) {
                 sr.hitAnObject = true;
                 tMin = hr.getT();
-                sr.color = obj.getColor();
+                sr.material = obj.getMaterial();
+                normal = sr.normal;
+                localHitPoint = sr.localHitPoint;
             }
+        }
+
+        if (sr.hitAnObject) {
+            sr.normal = normal;
+            sr.localHitPoint = localHitPoint;
         }
         return sr;
     }
 
     public Tracer getTracer() {
         return tracer;
+    }
+
+    public Light getAmbientLight() {
+        return ambientLight;
+    }
+
+    public void setAmbientLight(Light ambientLight) {
+        this.ambientLight = ambientLight;
+    }
+
+    public List<Light> getLights() {
+        return Collections.unmodifiableList(lights);
     }
 }
