920707ca79f964937bf5cecb15dbc95481c497d8
ViniGodoy <vinigodoy@gmail.com>
2013-11-12 00:03:46 -0200

    Shading support

--- src/br/com/vinigodoy/raytracer/utility/ShadeRec.java
+++ src/br/com/vinigodoy/raytracer/utility/ShadeRec.java
@@ -12,18 +12,63 @@ http://creativecommons.org/licenses/by-sa/2.5/br/
 package br.com.vinigodoy.raytracer.utility;
 
 
+import br.com.vinigodoy.raytracer.material.Material;
+import br.com.vinigodoy.raytracer.math.Ray;
 import br.com.vinigodoy.raytracer.math.Vector3;
 import br.com.vinigodoy.raytracer.scene.World;
 
-public class ShadeRec {
+public class ShadeRec implements Cloneable {
+    /**
+     * Indicate if the ray hit an object
+     */
     public boolean hitAnObject = false;
+
+    /**
+     * Nearest object material
+     */
+    public Material material;
+
+    /**
+     * World coordinates of hit point.
+     */
+    public Vector3 hitPoint = null;
+
+    //For attaching texture to objects
     public Vector3 localHitPoint = null;
+
+    /**
+     * Normal at hit point.
+     */
     public Vector3 normal = null;
-    public Vector3 color;
+
+    //For specular highlights
+    public Ray ray;
+
+    /**
+     * Recursion depth.
+     */
+    public int depth;
+
+    //For area lights
+    public Vector3 dir;
+
     public World world;
 
     public ShadeRec(World world) {
         this.world = world;
-        this.color = world.getBackgroundColor();
+    }
+
+    @Override
+    public ShadeRec clone() {
+        ShadeRec sr = new ShadeRec(world);
+        sr.hitAnObject = hitAnObject;
+        sr.material = material == null ? null : material.clone();
+        sr.hitPoint = hitPoint == null ? null : hitPoint.clone();
+        sr.localHitPoint = localHitPoint == null ? null : localHitPoint.clone();
+        sr.ray = ray == null ? null : new Ray(ray.getOrigin(), ray.getDirection());
+        sr.depth = depth;
+        sr.dir = dir == null ? null : dir.clone();
+        sr.normal = normal == null ? null : normal.clone();
+        return sr;
     }
 }
