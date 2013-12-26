6686097974408a65c2bfd0613e62b00de9edc345
ViniGodoy <vinigodoy@gmail.com>
2013-12-26 03:02:52 -0200

    Added shadow hit function

--- src/br/com/vinigodoy/raytracer/math/geometry/primitive/Box.java
+++ src/br/com/vinigodoy/raytracer/math/geometry/primitive/Box.java
@@ -132,6 +132,77 @@ public class Box implements GeometricObject {
 
     @Override
     public boolean shadow_hit(Ray ray, FloatRef tmin) {
+        double ox = ray.getOrigin().getX();
+        double oy = ray.getOrigin().getY();
+        double oz = ray.getOrigin().getZ();
+
+        double dx = ray.getDirection().getX();
+        double dy = ray.getDirection().getY();
+        double dz = ray.getDirection().getZ();
+
+        double tx_min, ty_min, tz_min;
+        double tx_max, ty_max, tz_max;
+
+        double a = 1.0 / dx;
+        if (a >= 0) {
+            tx_min = (x0 - ox) * a;
+            tx_max = (x1 - ox) * a;
+        } else {
+            tx_min = (x1 - ox) * a;
+            tx_max = (x0 - ox) * a;
+        }
+
+        double b = 1.0 / dy;
+        if (b >= 0) {
+            ty_min = (y0 - oy) * b;
+            ty_max = (y1 - oy) * b;
+        } else {
+            ty_min = (y1 - oy) * b;
+            ty_max = (y0 - oy) * b;
+        }
+
+        double c = 1.0 / dz;
+        if (c >= 0) {
+            tz_min = (z0 - oz) * c;
+            tz_max = (z1 - oz) * c;
+        } else {
+            tz_min = (z1 - oz) * c;
+            tz_max = (z0 - oz) * c;
+        }
+
+        //Find the largest entering value
+        double t0;
+
+        if (tx_min > ty_min) {
+            t0 = tx_min;
+        } else {
+            t0 = ty_min;
+        }
+        if (tz_min > t0) {
+            t0 = tz_min;
+        }
+
+        //Find the smallest exiting value
+        double t1;
+        if (tx_max < ty_max) {
+            t1 = tx_max;
+        } else {
+            t1 = ty_max;
+        }
+        if (tz_max < t1) {
+            t1 = tz_max;
+        }
+
+        if (t0 < t1 && t1 > K_EPSILON) {
+            if (t0 > K_EPSILON) {
+                tmin.value = (float) t0;
+            } else {
+                tmin.value = (float) t1;
+            }
+
+            return true;
+        }
+
         return false;
     }
 
